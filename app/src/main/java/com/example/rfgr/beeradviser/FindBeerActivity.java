9c7761bcf7959550351000a50fba7fc51e6c53ae
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-07-26 17:02:33 +0200

    Dalsza cześć ligiki aplikacji. Zakończenie aplikacji.

--- app/src/main/java/com/example/rfgr/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/rfgr/beeradviser/FindBeerActivity.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert(); //dodanie nowej instancji klasy BeerExpert jako prywatne pole klasy
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,16 @@ public class FindBeerActivity extends AppCompatActivity {
     //poniżej tworzę metodę która zostanie zaimplementowana po wciśnieciu guzika
     public void onClickFindBeer(View view) {
 
-        TextView brands = (TextView) findViewById(R.id.brands); //tworzę odwołanie do odnalezionego textView
-        Spinner color = (Spinner) findViewById(R.id.color); //tworzę odwołanie do spinnera
-        String beerType = String.valueOf(color.getSelectedItem()); //konwertuję wybranego spinnera na string
-        brands.setText(beerType); //podmieniam TextView na skonwertowanego spinnera
+        TextView brands = (TextView) findViewById(R.id.brands); //pobieram referencję do odnalezionego textView
+        Spinner color = (Spinner) findViewById(R.id.color); //pobieram referencję do spinnera
+        String beerType = String.valueOf(color.getSelectedItem()); //pobieram wartość wybraną w spinnerze i konwertuję ją na string
+        // brands.setText(beerType); //wyświetlam wybraną wartość
 
+        List<String> brandList = expert.getBrands(beerType); //używa klasy BeerExpert do pobrania listy array
+        StringBuilder brandsFormatted = new StringBuilder(); //konstruuje stringa w którym zostaną zapisane wartości z listy array
+        for (String brand : brandList) {
+            brandsFormatted.append(brand).append('\n'); //wyświetla każdą pozycję z listy array w nowym wierszu
+        }
+        brands.setText(brandsFormatted); //wyświetla wyniki
     }
 }
